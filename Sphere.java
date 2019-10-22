102b95ec3e8551010759366391885f65528e8691
ewang9 <56599425+ewang9@users.noreply.github.com>
2019-10-22 09:50:21 -0400

    Add sphere constructor
    Initializes with radius, SA and Volume calculated in constructor, put in inherited variables, inherited methods can be used to print them out

--- Sphere.java
+++ Sphere.java
@@ -1,5 +1,13 @@
 package modularityproject;
 
+import java.lang.Math;
+
 public class Sphere extends Shape3D{
     public double rad;
+    
+    public Sphere(double radius){
+        this.rad = radius;
+        this.SA = Math.pow(rad,2)*Math.PI*4;
+        this.volume = Math.pow(rad,3)*Math.PI*4/3;
+    }
 }
