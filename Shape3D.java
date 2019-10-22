f9429b02eaa0bba40342ed95af7b8f4e30dbfe76
ewang9 <56599425+ewang9@users.noreply.github.com>
2019-10-22 09:28:56 -0400

    Added print volume/SA

--- Shape3D.java
+++ Shape3D.java
@@ -1,6 +1,16 @@
 package modularityproject;
 
+import java.lang.Math;
+
 public class Shape3D {
     public double volume;
     public double SA;
+    
+    public void printVolume(){
+        System.out.println("The volume of this 3D shape is " + Math.round(volume));
+    }
+    
+    public void printSA(){
+        System.out.println("The surface area of this 3D shape is " + Math.round(SA));
+    }
 }
