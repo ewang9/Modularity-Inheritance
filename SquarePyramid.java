a10854154eb26e290a0bd9db1f51931818edb404
ewang9 <56599425+ewang9@users.noreply.github.com>
2019-10-24 10:10:26 -0400

    Update constructor, still WIP

--- SquarePyramid.java
+++ SquarePyramid.java
@@ -1,6 +1,21 @@
 package modularityproject;
 
+import java.lang.Math;
 public class SquarePyramid extends Pyramid{
-    public double bLength;
-    public double height;
+    public double bLength = -1;
+    
+    public SquarePyramid(double b, double h){
+        this.bLength = b;
+        this.height = h;
+        this.volume = bLength*bLength*height/3;
+        
+        this.SA = (Math.pow(bLength,2)) + ((Math.sqrt(Math.pow((bLength/2),2)+Math.pow(height,2)))*bLength*2);
+    }
+    
+    public SquarePyramid(){
+        
+    }
+    
+    
+    
 }
