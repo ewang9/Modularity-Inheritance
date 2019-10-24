b409eaa5a124354b181503ed5b20b4756b94b27d
ewang9 <56599425+ewang9@users.noreply.github.com>
2019-10-24 09:16:22 -0400

    Fixed initial 0 volume issue

--- Shape3D.java
+++ Shape3D.java
@@ -3,14 +3,29 @@ package modularityproject;
 import java.lang.Math;
 
 public class Shape3D {
-    public double volume;
-    public double SA;
+    public double volume = -1;
+    public double SA = -1;
+    
+    public Shape3D(double v, double sa){
+        this.volume = v;
+        this.SA = sa;
+    }
+    
+    public Shape3D(){
+        
+    }
     
     public void printVolume(){
+        if (volume>=0){
         System.out.println("The volume of this 3D shape is " + Math.round(volume));
+        }
+        else System.out.println("The volume of this 3D shape is unknown");
     }
     
     public void printSA(){
+        if (volume>=0){
         System.out.println("The surface area of this 3D shape is " + Math.round(SA));
+        }
+        else System.out.println("The surface area of this 3D shape is unknown");
     }
 }
